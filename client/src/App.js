6544ab5312dd698888389d4a1760a5084f2c0c2f
lucasz10 <lucas.zach10@gmail.com>
2022-11-13 15:49:34 -0700

    Set up React Routing, Pages, and Component organization

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Imported components
+import { Header, Footer } from '/components'
+
+// Imported pages for Routing
+import Login from './pages/Login';
+import CreateAccount from './pages/createAccount';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Header />
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Login />} 
+          />
+          <Route 
+            path="/createAccount" 
+            element={<CreateAccount />} 
+          />
+          <Route 
+            path="/:userId/home" 
+            element={<Home />} 
+          />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    
+}
+
+export default Login;
