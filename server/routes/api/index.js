ba2f279c822cfd73b31c7bef51053d739e1a836c
lucasz10 <lucas.zach10@gmail.com>
2022-11-14 20:40:09 -0700

    Updated routing. added hashing to user password through bcrypt

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const toppingRoutes = require('./Topping-routes');
+const pizzaRoutes = require('./Pizza-routes');
 
 router.use('/user', userRoutes);
+router.use('/topping', toppingRoutes);
+router.use('/pizza', pizzaRoutes);
 
 module.exports = router;
